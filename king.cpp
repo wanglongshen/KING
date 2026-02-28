5d710850396b998fd577d8879c575adc3e409d7a
wanglongshen <19532295223@163.com>
2026-02-28 11:31:15 +0800

    Update king.cpp
    update:workspace1

--- king.cpp
+++ king.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+    cout<<"test comflict 1"<<endl;
     cout<<"I am king.I am world."<<endl;
     cout<<"我是高能量的状态"<<endl;
     cout<<"我是铩羽，我是王龙神。我极度地接纳自我。我极度地认可自我。这就够了"<<endl;
