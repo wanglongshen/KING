5d77e84d524e60abc6125b6aa6260fbf70eeba1a
wanglongshen <19532295223@163.com>
2026-03-03 19:22:42 +0800

    Update HelloWorld.cpp
    test conflict2

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    cout<<"test conflict 2"<<endl;
     cout<<"Hello,World"<<endl;
     cout<<"我是铩羽，我是王龙神。我极度地接纳自我，我极度地认可自我。这就够了"<<endl;
     cout<<"我现在是最强哒。哈哈哈哈哈"<<endl;
